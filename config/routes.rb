3e927f8d26838bf2254cfa275bdb003f766c8c5c
Catharina <catha.ana.1994@gmail.com>
2016-08-25 12:01:36 -0300

    Adicao de nome no formulario de cadastro de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :usuarios
+  devise_for :usuarios, :controllers => { registrations: 'registrations' }
   resources :usuarios
   resources :participantes
   resources :encontros
